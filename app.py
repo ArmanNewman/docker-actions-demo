3814f5d751fd4a540eb186b34101f50cd2df86a5
Armando <41837595+ArmanNewman@users.noreply.github.com>
2022-06-16 21:23:51 -0500

    Update app.py
    Adds an emoji

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 name = "Armando Jiménez"
 os = "Mac-OS"
-print(f"Hi! My name is {name} and I developed this app in {os}, but you can run it everywhere :) \U0001F60A")
+print(f"Hi! My name is {name} and I developed this app in {os}, but you can run it everywhere :) \U0001F60A 😊")
